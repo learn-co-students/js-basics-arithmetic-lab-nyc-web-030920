70ae88ae3c03882a4f8a2b438790e2ee60bca661
Yahyagok <ygok17@hotmail.com>
2020-04-18 12:43:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+const var1 = 2;
+
+const var2 = 31;
+const multiply = var1 * var2;
+
+
+const random = Math.random() * 10; 
+
+const mod = 16 % 6;
+
+const max = Math.max(1,2,20);
